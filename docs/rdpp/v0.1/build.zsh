40bb4991807839ae17050f1d3d810453ee375702
Ronald Koenig <ronald.koenig@spherity.com>
2026-02-12 14:31:50 +0100

    feat: add ontology to section data model

--- docs/rdpp/v0.1/build.zsh
+++ docs/rdpp/v0.1/build.zsh
@@ -1,6 +1,7 @@
 cp ../../../src/template.html template.html
 cp ../../../src/vocabulary.yml vocabulary.yml
 npx yml2vocab -c
+sed -i.bak '1,/# Context files and their mentions/!d' vocabulary.ttl
 
 cp ../../../src/class-diagram.mermaid class-diagram.mermaid
 npx -p @mermaid-js/mermaid-cli mmdc -i class-diagram.mermaid -o class-diagram.svg
